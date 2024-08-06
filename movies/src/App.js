d15aed95630b5c69e8691bc42ef3baf268e2cb54
Brian <brijump3@gmail.com>
2024-08-06 11:55:08 -0500

    level 1 baby!

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
+import {useState, useEffect} from 'react'
 import './App.css';
 
-const movies = [
-  {title: 'Mean Girls'},
-  {title: 'Hackers'},
-  {title: 'The Grey'},
-  {title: 'Sunshine'},
-  {title: 'Ex Machina'},
-];
+
+
 
 function App() {
+  const [moviesList, setMoviesList] = useState([]);
+
+  useEffect(() => {
+   GetMovies()
+  }, [])
+
+  const GetMovies = async () => {
+    try {
+      const response = await fetch(`http://localhost:8080/movies`);
+      if (response.ok) {
+        const data = await response.json();
+        setMoviesList(data)
+        console.log(moviesList)
+      } else {
+        console.error('Dang brah, stuff is messed up for real')
+      }
+    } catch (error) {
+      console.error('Error here too dudes')
+    }
+  }
+
+
 
   return (
     <div className="App">
       <h1>Movies:</h1>
-      {movies.map((x) => {
-      return (
-        <>
-        <h5>{x.title}</h5>
-        </>
-      )
-    })}
+      {/* start conditional render */}
+      {moviesList.length > 0 ? (
+        moviesList.map((movie) => (
+          <div key = {movie.id}>
+            <h5>{movie.title}</h5>
+            </div>
+        ))
+      ) : (
+        <p>No movies available</p>
+      )}
+      {/* end conditional render */}
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+const cors = require('cors')
 const app = express();
 const port = 8080;
 const knex = require('knex')(
     require('./knexfile.js')[process.env.NODE_ENV||'development'])
 
 app.use(express.json());
+app.use(cors())
 
 // app.get('/', (req, res) => res.send('Hello World!'))
 
