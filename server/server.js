0d9d050a9461d2d5554df54ad46c1b62944300ab
Brian <brijump3@gmail.com>
2024-08-06 11:20:10 -0500

    whoopsie, forgot that there nodemon

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const port = 8080;
 const knex = require('knex')(
@@ -6,12 +7,18 @@ const knex = require('knex')(
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/movies', function(req, res) {
   knex('movies')
     .select('*')
     .then(data => res.status(200).json(data))
+    .catch(err =>
+      res.status(404).json({
+        message:
+          'The data you are looking for could not be found. Please try again'
+      })
+    );
 })
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
